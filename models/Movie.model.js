f05ee958b6415230651418e669d7a6bc0b2ad150
Zefanja Timorason <z.timorason@gmail.com>
2023-01-07 18:47:29 +0100

    Attempted iteration 5 not sure about line 7 in the movieModel

---
+++ models/Movie.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const movieModel = new Schema({
+  title: String,
+  genre: String,
+  plot: String,
+  cast: [{ type: Schema.Types.ObjectId, ref: "Celebrity" }],
+});
+
+module.exports = model("movies", movieModel);
